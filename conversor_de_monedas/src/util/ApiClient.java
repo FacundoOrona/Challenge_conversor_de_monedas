1e464f363b50bf27243e685393de1d4e715eea6e
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-11-12 17:43:06 -0300

    Update ApiClient.java

--- conversor_de_monedas/src/util/ApiClient.java
+++ conversor_de_monedas/src/util/ApiClient.java
@@ -1,4 +1,19 @@
 package util;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ApiClient {
+
+    public static String get(String url) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
 }
