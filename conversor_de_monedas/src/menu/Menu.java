a8d9683aa48ae31cd04b59ccec0eb98a176115af
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-11-12 17:43:11 -0300

    Update Menu.java

--- conversor_de_monedas/src/menu/Menu.java
+++ conversor_de_monedas/src/menu/Menu.java
@@ -1,4 +1,17 @@
 package menu;
 
+import servicios.ConversorService;
+
+import java.util.Scanner;
+
 public class Menu {
+    private final ConversorService conversorService = new ConversorService();
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void mostrar() {
+        try {
+            
+        }
+    }
+
 }
