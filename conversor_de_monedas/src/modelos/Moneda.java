f2bc12ddd9e77abfcda3083844a3a0c67603c887
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-11-12 17:43:09 -0300

    Update Moneda.java

--- conversor_de_monedas/src/modelos/Moneda.java
+++ conversor_de_monedas/src/modelos/Moneda.java
@@ -1,4 +1,20 @@
 package modelos;
 
 public class Moneda {
+    private String codigo;
+    private double valor;
+
+    public Moneda(String codigo, double valor) {
+        this.codigo = codigo;
+        this.valor = valor;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
 }
