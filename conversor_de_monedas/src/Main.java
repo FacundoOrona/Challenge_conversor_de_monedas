4747d273da8eef5366b3dd9750c2249e1caa17a7
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-11-12 16:52:25 -0300

    Update Main.java

--- conversor_de_monedas/src/Main.java
+++ conversor_de_monedas/src/Main.java
@@ -1,23 +1,32 @@
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String api_key = "c05a908faa935c534e31a0fe";
-        String url = "https://v6.exchangerate-api.com/v6/"+
-                api_key+"/pair/";
+        String apiKey = "c05a908faa935c534e31a0fe";
+        // Moneda base (por ejemplo USD)
+        String baseCurrency = "USD";
+        String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + baseCurrency;
 
         //Creacion de cliente
         HttpClient client = HttpClient.newHttpClient();
 
         //Cracion de solicitud
-        HttpRequest solicitud = HttpRequest.newBuilder()
+        HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .GET()
                 .build();
+
+        //Obteniendo Respuesta
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
