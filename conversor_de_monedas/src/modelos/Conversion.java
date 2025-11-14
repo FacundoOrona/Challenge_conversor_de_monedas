b7fcdd91c23d5afd45f70c7cee26b180de772cb2
FacundoOrona <166318237+FacundoOrona@users.noreply.github.com>
2025-11-14 14:47:04 -0300

    Create Conversion.java

---
+++ conversor_de_monedas/src/modelos/Conversion.java
@@ -0,0 +1,11 @@
+package modelos;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Conversion {
+    private String monedaOrigen;
+    private String monedaDestino;
+    private double monto;
+    private double resultado;
+    private LocalDateTime fecha;
